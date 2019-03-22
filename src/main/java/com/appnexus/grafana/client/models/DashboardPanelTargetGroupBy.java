1cce1e3c086bbc008ee4a0ed098cd4b815ce5595
Alexander Diehm <alexander.diehm@gmx.net>
2019-03-22 08:22:59 +0100

    updated model

--- src/main/java/com/appnexus/grafana/client/models/DashboardPanelTargetGroupBy.java
+++ src/main/java/com/appnexus/grafana/client/models/DashboardPanelTargetGroupBy.java
@@ -1,32 +1,30 @@
 /* Licensed under Apache-2.0 */
 package com.appnexus.grafana.client.models;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonValue;
-
+import java.util.List;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(fluent = true)
 public class DashboardPanelTargetGroupBy {
-	List<String> params;
-	Type type;
+  List<String> params;
+  Type type;
 
-	public enum Type {
-		TIME("time"),
-		TAG("tag"),
-		FILL("fill");
-		private final String value;
+  public enum Type {
+    TIME("time"),
+    TAG("tag"),
+    FILL("fill");
+    private final String value;
 
-		Type(String s) {
-			value = s;
-		}
+    Type(String s) {
+      value = s;
+    }
 
-		@JsonValue
-		public String value() {
-			return value;
-		}
-	}
+    @JsonValue
+    public String value() {
+      return value;
+    }
+  }
 }
--- src/main/java/com/appnexus/grafana/client/models/DashboardPanelTargetSelect.java
+++ src/main/java/com/appnexus/grafana/client/models/DashboardPanelTargetSelect.java
@@ -1,10 +1,8 @@
 /* Licensed under Apache-2.0 */
 package com.appnexus.grafana.client.models;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonValue;
-
+import java.util.List;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -13,45 +11,45 @@ import lombok.experimental.Accessors;
 public class DashboardPanelTargetSelect {
   List<String> params;
   Type type;
-  
-	public enum Type {
-		FIELD("field"),
-		BOTTOM("bottom"),
-		FIRST("first"),
-		LAST("last"),
-		MAX("max"),
-		MIN("min"),
-		PERCENTILE("percentile"),
-		TOP("top"),
-		COUNT("count"),
-		DISTINCT("distinct"),
-		INTEGRAL("integral"),
-		MEAN("mean"),
-		MEDIAN("median"),
-		MODE("mode"),
-		SUM("sum"),
-		MATH("math"),
-		ALIAS("alias"),
-		HOLT_WINTERS("holt_winters"),
-		HOLT_WINTERS_WITH_FIT("holt_winters_with_fit"),
-		DERIVATIVE("derivative"),
-		SPREAD("spread"),
-		NON_NEGATIVE_DERIVATIVE("non_negative_derivative"),
-		DIFFERENCE("difference"),
-		NON_NEGATIVE_DIFFERENCE("non_negative_difference"),
-		MOVING_AVERAGE("moving_average"),
-		CUMULATIVE_SUM("cumulative_sum"),
-		STDDEV("stddev"),
-		ELAPSED("elapsed");
-		private final String value;
 
-		Type(String s) {
-			value = s;
-		}
+  public enum Type {
+    FIELD("field"),
+    BOTTOM("bottom"),
+    FIRST("first"),
+    LAST("last"),
+    MAX("max"),
+    MIN("min"),
+    PERCENTILE("percentile"),
+    TOP("top"),
+    COUNT("count"),
+    DISTINCT("distinct"),
+    INTEGRAL("integral"),
+    MEAN("mean"),
+    MEDIAN("median"),
+    MODE("mode"),
+    SUM("sum"),
+    MATH("math"),
+    ALIAS("alias"),
+    HOLT_WINTERS("holt_winters"),
+    HOLT_WINTERS_WITH_FIT("holt_winters_with_fit"),
+    DERIVATIVE("derivative"),
+    SPREAD("spread"),
+    NON_NEGATIVE_DERIVATIVE("non_negative_derivative"),
+    DIFFERENCE("difference"),
+    NON_NEGATIVE_DIFFERENCE("non_negative_difference"),
+    MOVING_AVERAGE("moving_average"),
+    CUMULATIVE_SUM("cumulative_sum"),
+    STDDEV("stddev"),
+    ELAPSED("elapsed");
+    private final String value;
 
-		@JsonValue
-		public String value() {
-			return value;
-		}
-	}
+    Type(String s) {
+      value = s;
+    }
+
+    @JsonValue
+    public String value() {
+      return value;
+    }
+  }
 }
